91b4d06694532ab9e6e711bd9cf9263edc8b5832
proseuche <proseuche@hanmail.net>
2021-09-30 16:13:20 +0900

    Change Crossroad configuration

--- adaptive_crossroad/config.py
+++ adaptive_crossroad/config.py
@@ -3,8 +3,10 @@ import torch
 
 # Simulation Configuration
 FLOW_NUMBER = 80
-START_HOUR = 6
-END_HOUR = 24
+START_HOUR = 0
+END_HOUR = 48
+GRAPH_START = 24
+GRAPH_END = 48
 
 # Crossroad Configuration
 ENV_SAMPLES = 100
@@ -12,9 +14,9 @@ TICK_PER_POINT = 12
 TEN_SECOND_PER_TICK = 1
 TOTAL_TICK = 8640 // TEN_SECOND_PER_TICK
 DECISION_LENGTH = 9
-DEFAULT_DECISION = [2, 2, 2, 1, 1, 1]
-STRAIGHT_OUT = 300
-LEFT_OUT = 100
+DEFAULT_DECISION = [2, 1, 1, 1, 2, 2]
+STRAIGHT_OUT = 30
+LEFT_OUT = 10
 OUTPUT_FLOW = np.array([[STRAIGHT_OUT, LEFT_OUT, 0, 0, 0, 0, 0, 0],
                         [STRAIGHT_OUT, 0, STRAIGHT_OUT, 0, 0, 0, 0, 0],
                         [0, 0, STRAIGHT_OUT, LEFT_OUT, 0, 0, 0, 0],
@@ -23,14 +25,14 @@ OUTPUT_FLOW = np.array([[STRAIGHT_OUT, LEFT_OUT, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, STRAIGHT_OUT, LEFT_OUT]]) * TEN_SECOND_PER_TICK
 
 # Environment Configuration
-AStraight = np.array([29, 21, 15, 13, 18, 36, 67, 94, 94, 91, 85, 81, 76, 81, 82, 82, 84, 85, 83, 74, 66, 64, 55,
-                      42]) * 100 / 360 * TEN_SECOND_PER_TICK
-BStraight = np.array([31, 22, 17, 14, 16, 31, 53, 75, 83, 81, 79, 79, 76, 79, 81, 84, 87, 90, 88, 83, 75, 70, 60,
-                      46]) * 100 / 360 * TEN_SECOND_PER_TICK
-CStraight = np.array([8, 6, 5, 4, 5, 9, 21, 34, 37, 33, 30, 29, 26, 27, 27, 27, 26, 26, 24, 21, 19, 19, 16,
-                      13]) * 100 / 360 * TEN_SECOND_PER_TICK
-DStraight = np.array([10, 7, 5, 4, 5, 8, 14, 22, 26, 26, 26, 27, 26, 28, 29, 29, 30, 32, 31, 28, 24, 23, 19,
-                      15]) * 100 / 360 * TEN_SECOND_PER_TICK
+AStraight = np.array([61, 37, 19, 13, 28, 82, 175, 256, 256, 247, 229, 217,
+                      202, 217, 220, 220, 226, 229, 223, 196, 172, 166, 139, 100]) * 10 / 360 * TEN_SECOND_PER_TICK
+BStraight = np.array([67, 38, 23, 14, 20, 65, 141, 207, 221, 217, 210, 209,
+                      210, 217, 220, 236, 242, 257, 242, 233, 197, 182, 152, 110]) * 10 / 360 * TEN_SECOND_PER_TICK
+CStraight = np.array([202, 217, 220, 220, 226, 229, 223, 196, 172, 166, 139, 100,
+                      61, 37, 19, 13, 28, 82, 175, 256, 256, 247, 229, 217]) * 10 / 360 * TEN_SECOND_PER_TICK
+DStraight = np.array([210, 217, 220, 236, 242, 257, 242, 233, 197, 182, 152, 110,
+                      67, 38, 23, 14, 20, 65, 141, 207, 221, 217, 210, 209]) * 10 / 360 * TEN_SECOND_PER_TICK
 ALeft = AStraight / 4
 BLeft = BStraight / 4
 CLeft = CStraight / 4
--- adaptive_crossroad/main.py
+++ adaptive_crossroad/main.py
@@ -10,21 +10,29 @@ if __name__ == "__main__":
     start_tick = config.START_HOUR * 360 // config.TEN_SECOND_PER_TICK
     end_tick = config.END_HOUR * 360 // config.TEN_SECOND_PER_TICK
 
+    graph_start = config.GRAPH_START * 360 // config.TEN_SECOND_PER_TICK // config.DECISION_LENGTH // config.TICK_PER_POINT
+    graph_end = config.GRAPH_END * 360 // config.TEN_SECOND_PER_TICK // config.DECISION_LENGTH // config.TICK_PER_POINT
+
     for i in [84]:
-        targets = [('VN', 'VN'),
-                   ('DEFAULT', '')]
+        targets = []
+        for target in config.TACTICS:
+            targets.append((config.tactic_string(target), '', target))
+        # targets = [('DEFAULT', '')]
         # targets = [('SMC', 'SMC'),
         #            ('VN', 'VN'),
         #            ('DEFAULT', '')]
 
-        time = np.arange(config.START_HOUR, config.END_HOUR, config.TEN_SECOND_PER_TICK * config.DECISION_LENGTH * config.TICK_PER_POINT / 360)
+        time = np.arange(config.GRAPH_START, config.GRAPH_END, config.TEN_SECOND_PER_TICK * config.DECISION_LENGTH * config.TICK_PER_POINT / 360)
 
-        point_result_god = np.array(crossroad.run_crossroad('GOD', 'GOD', i, start_tick=start_tick, end_tick=end_tick))
+        point_result_god = np.array(crossroad.run_crossroad('GOD', 'GOD', i, start_tick=start_tick, end_tick=end_tick))[graph_start:graph_end]
         plt.plot(time, point_result_god, label='GOD')
 
         target_result = []
         for target in targets:
-            point_result = np.array(crossroad.run_crossroad(target[0], target[1], start_tick=start_tick, end_tick=end_tick))
+            # point_result = np.array(crossroad.run_crossroad(target[0], target[1], start_tick=start_tick,
+            #                                                 end_tick=end_tick))[graph_start:graph_end]
+            point_result = np.array(crossroad.run_crossroad(target[0], target[1], start_tick=start_tick,
+                                                            end_tick=end_tick, default_decision=target[2]))[graph_start:graph_end]
             plt.plot(time, point_result, label=target[0])
             target_result.append(point_result - point_result_god)
 
@@ -32,7 +40,8 @@ if __name__ == "__main__":
         plt.legend(loc='upper left')
         plt.xlabel('Hour')
         plt.ylabel('Number of Cars')
-        plt.xticks([6, 12, 18, 24], [6, 12, 18, 24])
+        plt.xticks(list(range(config.GRAPH_START, config.GRAPH_END + 1, 6)),
+                   list(range(config.GRAPH_START, config.GRAPH_END + 1, 6)))
         plt.savefig('../figure/' + FIGURE_NAME + '.png', dpi=300)
         plt.show()
         plt.close()
@@ -40,11 +49,13 @@ if __name__ == "__main__":
         if len(targets) > 0:
             for j in range(len(target_result)):
                 plt.plot(time, target_result[j], label=targets[j][0])
+                print("%s: %1.4f" % (targets[j][0], sum(target_result[j])))
 
             plt.title('Time - Diff of Number of Waiting Cars')
             plt.legend(loc='upper left')
             plt.xlabel('Hour')
             plt.ylabel('Number of Cars')
-            plt.xticks([6, 12, 18, 24], [6, 12, 18, 24])
+            plt.xticks(list(range(config.GRAPH_START, config.GRAPH_END + 1, 6)),
+                       list(range(config.GRAPH_START, config.GRAPH_END + 1, 6)))
             plt.savefig('../figure/' + FIGURE_NAME + '_diff.png', dpi=300)
             plt.show()
