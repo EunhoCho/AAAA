e2658997fe6585dcb59a48c88aa671b759a2809a
proseuche <proseuche@hanmail.net>
2021-10-19 12:13:30 +0900

    Rl

--- adaptive_crossroad/trainRL.py
+++ adaptive_crossroad/trainRL.py
@@ -42,15 +42,16 @@ if __name__ == "__main__":
 
     for i in tqdm(range(config.AN_EPISODES)):
         flow = crossroad.read_flow(i)
-        state = np.array([0] * 9)
+        state = np.array([0] * (1 + config.WAYS))
         for j in range(config.TOTAL_TICK // config.DECISION_LENGTH):
             state_tensor = torch.FloatTensor([state]).to(config.AN_DEVICE)
             tactic = value_net.select_tactic(state_tensor)
 
             sim_result = crossroad.sim_run_crossroad(config.DECISION_LENGTH, config.TACTICS[tactic],
-                                                     flow[j * 9:(j + 1) * 9], state[1:])
+                                                     flow[j * config.DECISION_LENGTH:(j + 1) * config.DECISION_LENGTH],
+                                                     state[1:])
 
-            next_state = [(j + 1) * 9, *sim_result[-1]]
+            next_state = [(j + 1) * config.DECISION_LENGTH, *sim_result[-1]]
             reward = sum(sum(sim_result))
 
             memory.push(state_tensor, tactic, torch.FloatTensor([reward]).to(config.AN_DEVICE),
